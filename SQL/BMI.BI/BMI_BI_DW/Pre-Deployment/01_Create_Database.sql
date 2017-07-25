647352a9e7ec7f0c357fa00a3408f472d26171c7
gregmlucas <greg.lucas@icopal.com>
2017-07-25 16:10:43 +0100

    Added BMI_BI_DW package (permissions changes only)

--- SQL/BMI.BI/BMI_BI_DW/Pre-Deployment/01_Create_Database.sql
+++ SQL/BMI.BI/BMI_BI_DW/Pre-Deployment/01_Create_Database.sql
@@ -8,5 +8,3 @@ begin
 	create database [$(DatabaseName)] collate Latin1_General_BIN; -- MODIFY THIS STATEMENT TO SPECIFY A COLLATION FOR YOUR DATABASE
 end
 go
-
-:r "..\..\CreateDatabaseScripts\BMI_BI_DW Database.Options.sql"

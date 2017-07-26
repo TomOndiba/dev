87286b61e710dad5552c0f540fb7fefd4d1c446c
RNazir <razia.nazir@icopal.com>
2017-07-26 16:46:43 +0100

    added user scripts for permission, users  and merge.

---
+++ SQL/BMI.BI/CreateRoleScripts/BMI_BAT_MAN Roles and Permissions.sql
@@ -0,0 +1,211 @@
+--!
+--! BMI_BAT_MAN Roles and Permissions.sql
+--!
+
+--!
+--!
+--!
+if schema_id('log4') is not null grant execute on schema :: [log4] to [public];
+if schema_id('log4Utils') is not null grant execute on schema :: [log4Utils] to [public];
+go
+
+if not exists (select * from sys.database_principals where type = 'R' and name = 'IcsDataFeeders')
+	exec(N'create role [IcsDataFeeders] authorization [dbo];') ;
+go
+
+grant select on schema :: [INFORMATION_SCHEMA] to [IcsDataFeeders];
+grant select on schema :: [dbo] to [IcsDataFeeders];
+go
+if schema_id('IcsApp') is not null
+	exec (N'grant execute, select on schema :: [IcsApp] to [IcsDataFeeders];');
+go
+
+--grant execute on dbo.stubprocedure to IcsDataFeeders
+--if objectpropertyex(object_id(N'utils.LoadDateDimension'), N'IsProcedure') is not null exec (N'grant exec on utils.LoadDateDimension to [BatchManagers];') ;
+
+--!
+--! Create a batch managers role to allow support users to change selected load-related metadata etc.
+--!
+
+if objectpropertyex(object_id(N'dbo.ProcessRunStart'), N'IsProcedure') is not null 
+grant execute on [dbo].[ProcessRunStart] to [IcsDataFeeders]
+go
+
+if objectpropertyex(object_id(N'dbo.ProcessRunEnd'), N'IsProcedure') is not null 
+grant execute on [dbo].[ProcessRunEnd] to [IcsDataFeeders]
+
+go
+if objectpropertyex(object_id(N'dbo.ThreadRunEnd'), N'IsProcedure') is not null 
+grant execute on [dbo].[ThreadRunEnd] to [IcsDataFeeders]
+
+go
+if objectpropertyex(object_id(N'dbo.ThreadRunStart'), N'IsProcedure') is not null 
+grant execute on [dbo].[ThreadRunStart] to [IcsDataFeeders]
+
+go
+if objectpropertyex(object_id(N'dbo.SubProcessRunEnd'), N'IsProcedure') is not null 
+grant execute on [dbo].[SubProcessRunEnd] to [IcsDataFeeders]
+
+go
+if objectpropertyex(object_id(N'dbo.SubProcessRunStart'), N'IsProcedure') is not null 
+grant execute on [dbo].[SubProcessRunStart] to [IcsDataFeeders]
+
+go
+if objectpropertyex(object_id(N'dbo.SubProcessCheckThreads'), N'IsProcedure') is not null 
+grant execute on [dbo].[SubProcessCheckThreads] to [IcsDataFeeders]
+
+
+if not exists (select * from sys.database_principals where type = 'R' and name = 'BatchManagers')
+	exec(N'create role [BatchManagers] authorization [dbo];') ;
+go
+
+if schema_id('log4Private') is not null exec (N'grant select on schema :: [log4Private] to [BatchManagers];') ;
+if schema_id('privy') is not null exec (N'grant select on schema :: [privy] to [BatchManagers];') ;
+go
+
+--!
+--! Create a role to allow read-only access to the actual code
+--!
+if not exists (select * from sys.database_principals where type = 'R' and name = 'CodeReviewers')
+	exec(N'create role [CodeReviewers] authorization [dbo];') ;
+go
+grant view definition to [CodeReviewers];
+go
+
+--!
+--! Create the super-user role used for deployment in all environments
+--!
+if not exists (select * from sys.database_principals where type = 'R' and name = 'DeploymentManagers')
+	exec(N'create role [DeploymentManagers] authorization [dbo];') ;
+go
+exec sp_addrolemember N'db_datareader', N'DeploymentManagers' ;
+exec sp_addrolemember N'db_datawriter', N'DeploymentManagers' ;
+exec sp_addrolemember N'db_owner', N'DeploymentManagers' ;
+go
+
+--!
+--! Create any remaining environment-specific roles
+--!
+if not exists (select * from sys.database_principals where type = 'R' and name = 'SqlDevelopersDEV')
+	exec(N'create role [SqlDevelopersDEV] authorization [dbo];') ;
+if not exists (select * from sys.database_principals where type = 'R' and name = 'SqlDevelopersTEST')
+	exec(N'create role [SqlDevelopersTEST] authorization [dbo];') ;
+if not exists (select * from sys.database_principals where type = 'R' and name = 'SqlDevelopersPROD')
+	exec(N'create role [SqlDevelopersPROD] authorization [dbo];') ;
+go
+
+grant execute to [SqlDevelopersDEV];
+go
+exec sp_addrolemember N'db_datareader', N'SqlDevelopersDEV' ;
+exec sp_addrolemember N'db_datawriter', N'SqlDevelopersDEV' ;
+exec sp_addrolemember N'db_owner', N'SqlDevelopersDEV' ;
+go
+
+grant showplan to [SqlDevelopersTEST]
+go
+grant execute to [SqlDevelopersTEST];
+go
+exec sp_addrolemember N'db_datareader', N'SqlDevelopersTEST' ;
+exec sp_addrolemember N'db_datawriter', N'SqlDevelopersTEST' ;
+exec sp_addrolemember N'db_ddladmin', N'SqlDevelopersTEST' ;
+go
+
+grant view definition to [SqlDevelopersPROD];
+go
+grant showplan to [SqlDevelopersPROD]
+go
+exec sp_addrolemember N'db_datareader', N'SqlDevelopersPROD' ;
+go
+
+
+--!
+--!
+--!
+if not exists (select * from sys.database_principals where type = 'R' and name = 'EtlDevelopersDEV')
+	exec(N'create role [EtlDevelopersDEV] authorization [dbo];') ;
+if not exists (select * from sys.database_principals where type = 'R' and name = 'EtlDevelopersTEST')
+	exec(N'create role [EtlDevelopersTEST] authorization [dbo];') ;
+if not exists (select * from sys.database_principals where type = 'R' and name = 'EtlDevelopersPROD')
+	exec(N'create role [EtlDevelopersPROD] authorization [dbo];') ;
+go
+
+grant view definition to [EtlDevelopersDEV];
+go
+grant showplan to [EtlDevelopersDEV]
+go
+exec sp_addrolemember N'db_datareader', N'EtlDevelopersDEV' ;
+exec sp_addrolemember N'CodeReviewers', N'EtlDevelopersDEV' ;
+go
+if objectpropertyex(object_id(N'dbo.StubResultSet'), N'IsUserTable') is not null exec (N'grant select,update on dbo.StubResultSet to [EtlDevelopersDEV];') ;
+
+if objectpropertyex(object_id(N'dbo.ProcessRunStart'), N'IsProcedure') is not null 
+grant execute on [dbo].[ProcessRunStart] to EtlDevelopersDEV
+go
+
+if objectpropertyex(object_id(N'dbo.ProcessRunEnd'), N'IsProcedure') is not null 
+grant execute on [dbo].[ProcessRunEnd] to EtlDevelopersDEV
+go
+
+if objectpropertyex(object_id(N'dbo.ThreadRunEnd'), N'IsProcedure') is not null
+grant execute on [dbo].[ThreadRunEnd] to EtlDevelopersDEV
+go
+
+
+if objectpropertyex(object_id(N'dbo.ThreadRunStart'), N'IsProcedure') is not null 
+grant execute on [dbo].[ThreadRunStart] to EtlDevelopersDEV
+go
+
+
+if objectpropertyex(object_id(N'dbo.SubProcessRunEnd'), N'IsProcedure') is not null
+grant execute on [dbo].[SubProcessRunEnd] to EtlDevelopersDEV
+go
+
+
+if objectpropertyex(object_id(N'dbo.SubProcessRunStart'), N'IsProcedure') is not null 
+grant execute on [dbo].[SubProcessRunStart] to EtlDevelopersDEV
+go
+
+if objectpropertyex(object_id(N'dbo.SubProcessCheckThreads'), N'IsProcedure') is not null
+grant execute on [dbo].[SubProcessCheckThreads] to EtlDevelopersDEV
+
+--if objectpropertyex(object_id(N'utils.LoadDateDimension'), N'IsProcedure') is not null exec (N'grant exec on utils.LoadDateDimension to [EtlDevelopersDEV];') ;
+
+grant view definition to [EtlDevelopersDEV];
+go
+grant showplan to [EtlDevelopersTEST]
+go
+exec sp_addrolemember N'db_datareader', N'EtlDevelopersTEST' ;
+go
+
+grant view definition to [EtlDevelopersPROD];
+go
+if schema_id('privy') is not null exec (N'grant select on schema :: [privy] to [EtlDevelopersPROD];');
+go
+
+--!
+--!
+--!
+if not exists (select * from sys.database_principals where type = 'R' and name = 'BusinessAnalystsDEV')
+	exec(N'create role [BusinessAnalystsDEV] authorization [dbo];') ;
+if not exists (select * from sys.database_principals where type = 'R' and name = 'BusinessAnalystsTEST')
+	exec(N'create role [BusinessAnalystsTEST] authorization [dbo];') ;
+if not exists (select * from sys.database_principals where type = 'R' and name = 'BusinessAnalystsPROD')
+	exec(N'create role [BusinessAnalystsPROD] authorization [dbo];') ;
+go
+
+grant view definition to [BusinessAnalystsDEV];
+go
+exec sp_addrolemember N'db_datareader', N'BusinessAnalystsDEV' ;
+go
+
+grant view definition to [BusinessAnalystsTEST];
+go
+exec sp_addrolemember N'db_datareader', N'BusinessAnalystsTEST' ;
+go
+
+grant view definition to [BusinessAnalystsPROD];
+go
+if schema_id('privy') is not null exec (N'grant select on schema :: [privy] to [BusinessAnalystsPROD];');
+
+go
+
---
+++ SQL/BMI.BI/CreateUserScripts/BMI_BAT_MAN Users.sql
@@ -0,0 +1,541 @@
+--!
+--! BMI_BAT_MAN Users.sql
+--!
+set noexec off;
+go
+
+set nocount on
+go
+set xact_abort on
+go
+set transaction isolation level serializable
+go
+begin transaction
+go
+
+/*
+select * from sys.sysusers;
+select * from sys.database_principals;
+*/
+
+--=====================================================================================================================
+--! START OF USER-EDITABLE SECTION
+--=====================================================================================================================
+
+if object_id(N'tempdb..#Servers') is not null drop table #Servers;
+go
+create table #Servers
+(
+  ServerName sysname not null
+, Environment varchar(8) not null
+, primary key (ServerName, Environment)
+) ;
+go
+
+if object_id(N'tempdb..#Roles') is not null drop table #Roles;
+go
+create table #Roles
+(
+  RoleName sysname not null
+, Environment varchar(8) not null
+, primary key (RoleName, Environment)
+) ;
+go
+
+if object_id(N'tempdb..#Users') is not null drop table #Users;
+go
+create table #Users
+(
+  IsActive tinyint not null
+, Environment varchar(8) not null
+, LoginName sysname not null
+, DefaultSchema sysname not null
+, DoRevokeDatabaseAccess bit not null
+, DoGrantDatabaseAccess bit not null
+, primary key (LoginName, Environment)
+) ;
+go
+
+if object_id(N'tempdb..#RoleMembers') is not null drop table #RoleMembers;
+go
+create table #RoleMembers
+(
+  UserName sysname not null
+, RoleName sysname not null
+, primary key (UserName, RoleName)
+) ;
+go
+
+--! Actions
+declare @_DoDropInactiveUsers bit = 1 ;
+declare @_DoDropAllUsers bit = 1 ;
+declare @_DoAddDatabaseUsers bit = 1 ;
+declare @_DoAddRoleMembers bit = 1 ;
+declare @_Debug bit = 1 ;
+
+insert #Servers (ServerName, Environment)
+values
+	  ('bmibidwh'				, 'ALL')
+	, ('bmibidwh'				, 'DEV')
+	, ('TEST_NOT_IMPLEMENTED'	, 'ALL')
+	, ('TEST_NOT_IMPLEMENTED'	, 'TEST')
+	, ('UAT_NOT_IMPLEMENTED'	, 'ALL')
+	, ('UAT_NOT_IMPLEMENTED'	, 'PROD')
+	, ('bmidwhprod'				, 'ALL')
+	, ('bmidwhprod'				, 'PROD')
+;
+
+insert #Roles (Environment, RoleName)
+values
+	  ('ALL'	, 'BatchManagers')
+	, ('ALL'	, 'CodeReviewers')
+	, ('ALL'	, 'IcsDataFeeders')
+	, ('ALL'	, 'DeploymentManagers')
+	, ('DEV'	, 'BusinessAnalysts')
+	, ('TEST'	, 'BusinessAnalysts')
+	, ('PROD'	, 'BusinessAnalysts')
+	, ('DEV'	, 'EtlDevelopers')
+	, ('TEST'	, 'EtlDevelopers')
+	, ('PROD'	, 'EtlDevelopers')
+	, ('DEV'	, 'SqlDevelopers')
+	, ('TEST'	, 'SqlDevelopers')
+	, ('PROD'	, 'SqlDevelopers')
+
+insert #Users (IsActive, Environment, LoginName, DefaultSchema, DoRevokeDatabaseAccess, DoGrantDatabaseAccess)
+values (0, '', '', '', 0, 0)
+	--! Application Users
+	, (1	, 'DEV'		, 'InfaDev'									, 'dbo'	, 1	, 1)
+	, (1	, 'PROD'	, 'InfaProd'								, 'dbo'	, 1	, 1)
+	, (1	, 'DEV'		, 'BmiBiDwFeedDev'							, 'dbo'	, 1	, 1)
+	, (1	, 'PROD'	, 'BmiBiDwFeedProd'							, 'dbo'	, 1	, 1)
+	, (1	, 'DEV'		, 'BmiBiMartFeedDev'						, 'dbo'	, 1	, 1)
+	, (1	, 'PROD'	, 'BmiBiMartFeedProd'						, 'dbo'	, 1	, 1)
+
+	--! SQL Developers
+--! DO NOT revoke database access for this user to avoid breaking other DBA-level permissions
+--	, (1	, 'DEV'		, 'greg.lucas@icopal.com'			, 'dbo'	, 0	, 1)
+--	, (1	, 'PROD'	, 'greg.lucas@icopal.com'			, 'dbo'	, 0	, 1)
+	, (1	, 'DEV'		, 'razia.nazir@icopal.com'			, 'dbo'	, 0	, 1)
+	, (1	, 'PROD'	, 'razia.nazir@icopal.com'			, 'dbo'	, 0	, 1)
+
+	--! ETL Developers
+	, (1	, 'DEV'		, 'Vincent.Mitchell@icopal.com'				, 'dbo'	, 1	, 1)
+	, (1	, 'PROD'	, 'Vincent.Mitchell@icopal.com'				, 'dbo'	, 1	, 1)
+	, (1	, 'DEV'		, 'Jason.Bogart@icopal.com'					, 'dbo'	, 1	, 1)
+	, (1	, 'PROD'	, 'Jason.Bogart@icopal.com'					, 'dbo'	, 1	, 1)
+	, (1	, 'DEV'		, 'Jarek.Czujak@icopal.com'					, 'dbo'	, 1	, 1)
+	, (1	, 'PROD'	, 'Jarek.Czujak@icopal.com'					, 'dbo'	, 1	, 1)
+
+	--! App/Report Developers
+	, (1	, 'DEV'		, 'tom.manville@icopal.com'					, 'dbo'	, 1	, 1)
+	, (1	, 'PROD'	, 'tom.manville@icopal.com'					, 'dbo'	, 1	, 1)
+
+	--! Business Analysts/Testers/Management
+	, (1	, 'DEV'		, 'mark.bolton@icopal.com'					, 'dbo'	, 1	, 1)
+	, (1	, 'PROD'	, 'mark.bolton@icopal.com'					, 'dbo'	, 1	, 1)
+
+	---------------------------------------------------------------------------------------------------
+
+	--! Expired User Accounts
+	, (0	, 'DEV'		, 'Jonathan.Holdcroft@icopal.com'			, 'dbo'	, 1	, 0)
+	, (0	, 'PROD'	, 'Jonathan.Holdcroft@icopal.com'			, 'dbo'	, 1	, 0)
+;
+
+insert #RoleMembers (UserName, RoleName)
+values ('', '')
+	--! SQL Developers
+	, ('greg.lucas@icopal.com'					, 'SqlDevelopers')
+	, ('greg.lucas@icopal.com'					, 'BatchManagers')
+	, ('greg.lucas@icopal.com'					, 'CodeReviewers')
+	, ('razia.nazir@icopal.com'					, 'SqlDevelopers')
+	, ('razia.nazir@icopal.com'					, 'BatchManagers')
+	, ('razia.nazir@icopal.com'					, 'CodeReviewers')
+
+	--! ETL Developers
+	, ('Vincent.Mitchell@icopal.com'			, 'EtlDevelopers')
+	, ('Vincent.Mitchell@icopal.com'			, 'BatchManagers')
+	, ('Vincent.Mitchell@icopal.com'			, 'CodeReviewers')
+	, ('Jason.Bogart@icopal.com'				, 'EtlDevelopers')
+	, ('Jason.Bogart@icopal.com'				, 'BatchManagers')
+	, ('Jason.Bogart@icopal.com'				, 'CodeReviewers')
+	, ('Jarek.Czujak@icopal.com'				, 'EtlDevelopers')
+	, ('Jarek.Czujak@icopal.com'				, 'BatchManagers')
+	, ('Jarek.Czujak@icopal.com'				, 'CodeReviewers')
+
+	--! App/Report Developers
+	, ('tom.manville@icopal.com'				, 'BusinessAnalysts')
+
+	--! Business Analysts/Testers/Management
+	, ('mark.bolton@icopal.com'					, 'BusinessAnalysts')
+
+	--! Other Roles
+	, ('greg.lucas@icopal.com'					, 'DeploymentManagers')
+	, ('razia.nazir@icopal.com'					, 'DeploymentManagers')
+
+	, ('InfaDev'								, 'IcsDataFeeders')
+	, ('InfaProd'								, 'IcsDataFeeders')
+	, ('BmiBiDwFeedDev'							, 'IcsDataFeeders')
+	, ('BmiBiDwFeedProd'						, 'IcsDataFeeders')
+	, ('BmiBiMartFeedDev'						, 'IcsDataFeeders')
+	, ('BmiBiMartFeedProd'						, 'IcsDataFeeders')
+
+	, (''		, 'DeploymentManagers')
+	, (''		, 'CodeReviewers')
+	, (''		, 'IcsDataFeeders')
+;
+
+--=====================================================================================================================
+--! END OF USER-EDITABLE SECTION
+--=====================================================================================================================
+
+declare @_Error int = 0 ;
+declare @_Step varchar (128)
+declare @_Message varchar  (512)
+declare @_LoopCount int
+declare @_CursorStatus smallint ;
+
+--! Variables
+declare @_ServerName sysname = @@servername ;
+declare @_CurrentEnvironment varchar(8) ;
+declare @_DatabaseName sysname = db_name() ;
+declare @_LoginName sysname ;
+declare @_DefaultSchema sysname ;
+declare @_DatabaseUserName sysname ;
+declare @_DatabaseRoleName sysname ;
+declare @_SqlCommand nvarchar(max);
+
+begin try
+	--!
+	--! Validations
+	--!
+	if not exists (select 1 from #Servers where ServerName = @_ServerName)
+		raiserror ('Current server name <<%s>> is not in the expected list of servers', 16, 1, @_ServerName) with nowait;
+
+	--!
+	--! What environment are we operating in?
+	--!
+	select @_CurrentEnvironment = Environment from #Servers where ServerName = @_ServerName and Environment <> 'ALL'
+
+	raiserror('CURRENT SERVER: %s;  CURRENT ENVIRONMENT: %s', 0, 1, @_ServerName, @_CurrentEnvironment) with nowait ;
+
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+DropDatabaseUsers:
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	set @_Step = 'Drop Database Users' ;
+	if @_DoDropAllUsers = 1 or @_DoDropInactiveUsers = 1
+		begin
+			if @_Debug = 1
+				select distinct
+					  @_Step as [Step]
+					, u.LoginName
+					, u.Environment
+					, s.ServerName
+					, u.IsActive
+				from
+					#Users as u
+				inner join #Servers as s
+					on s.Environment = u.Environment
+				inner join sys.database_principals as dp
+					on dp.name collate Latin1_General_BIN = u.LoginName collate Latin1_General_BIN
+				where
+						len(u.LoginName) > 0
+					and s.ServerName = @_ServerName
+					and u.DoRevokeDatabaseAccess = 1
+					and u.IsActive = case
+										when @_DoDropAllUsers = 1 then u.IsActive
+										when @_DoDropInactiveUsers = 1 then 0
+										else 99
+									  end
+
+			begin try
+				declare dropUsersCursor cursor for
+					select
+						distinct u.LoginName
+					from
+						#Users as u
+					inner join #Servers as s
+						on s.Environment = u.Environment
+					inner join sys.database_principals as dp
+						on dp.name collate Latin1_General_BIN = u.LoginName collate Latin1_General_BIN
+					where
+							len(u.LoginName) > 0
+						and s.ServerName = @_ServerName
+						and u.DoRevokeDatabaseAccess = 1
+						and u.IsActive = case
+											when @_DoDropAllUsers = 1 then u.IsActive
+											when @_DoDropInactiveUsers = 1 then 0
+											else 99
+										  end
+
+				open dropUsersCursor
+
+				fetch next from dropUsersCursor into @_DatabaseUserName
+
+				while @@fetch_status = 0
+					begin
+						set @_SqlCommand = 'drop user [' + @_DatabaseUserName + '];' ;
+
+						raiserror('Running CMD: <<< %s >>>...', 0, 1, @_SqlCommand) with nowait ;
+
+						exec (@_SqlCommand) ;
+
+						set @_SqlCommand = null ;
+
+						fetch next from dropUsersCursor into @_DatabaseUserName
+					end
+
+				close dropUsersCursor
+
+				deallocate dropUsersCursor
+			end try
+			begin catch
+				select error_message() as [ERROR_MESSAGE];
+				
+				set @_CursorStatus = cursor_status('global','dropUsersCursor');
+				select @_CursorStatus as [CURSOR_STATUS];
+
+				if @_CursorStatus >- 1
+					begin
+						close dropUsersCursor;
+						deallocate dropUsersCursor;
+					end
+				else if @_CursorStatus in (-1, -2)
+					deallocate dropUsersCursor;
+
+				select cursor_status('global','dropUsersCursor')  as [CURSOR_STATUS];
+
+				throw;
+			end catch
+
+		end
+	else
+		raiserror('[%s] step skipped as disabled', 0, 1, @_Step) ; 
+
+
+
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+AddDatabaseUsers:
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	set @_Step = 'Add Database Users' ;
+	if @_DoAddDatabaseUsers = 1
+		begin
+			if @_Debug = 1
+				select distinct
+					  @_Step as [Step]
+					, u.LoginName
+					, u.DefaultSchema
+					, u.Environment
+					, s.ServerName
+				from
+					#Users as u
+				inner join #Servers as s
+					on s.Environment = u.Environment
+				left join sys.database_principals as dp
+					on dp.name collate Latin1_General_BIN = u.LoginName collate Latin1_General_BIN
+				where
+						len(u.LoginName) > 0
+					and s.ServerName = @_ServerName
+					and s.Environment = @_CurrentEnvironment
+					and u.IsActive = 1
+					and u.DoGrantDatabaseAccess = 1
+					and dp.principal_id is null
+
+			begin try
+				declare addUsersCursor cursor for
+					select
+						distinct u.LoginName, u.DefaultSchema
+					from
+						#Users as u
+					inner join #Servers as s
+						on s.Environment = u.Environment
+					left join sys.database_principals as dp
+						on dp.name collate Latin1_General_BIN = u.LoginName collate Latin1_General_BIN
+					where
+							len(u.LoginName) > 0
+						and s.ServerName = @_ServerName
+						and u.IsActive = 1
+						and u.DoGrantDatabaseAccess = 1
+						and dp.principal_id is null
+
+				open addUsersCursor
+
+				fetch next from addUsersCursor into @_DatabaseUserName, @_DefaultSchema ;
+
+				while @@fetch_status = 0
+					begin
+						set @_SqlCommand = 'create user [' + @_DatabaseUserName + ']'
+							+ case
+								when @_DatabaseUserName like '%@icopal.com'
+									then ' from external provider'
+								else ' for login [' + @_DatabaseUserName + ']'
+							  end
+							+ ' with default_schema = [' + @_DefaultSchema + '];' ;
+
+						raiserror('Running CMD: <<< %s >>>...', 0, 1, @_SqlCommand) with nowait ;
+
+						exec (@_SqlCommand) ;
+
+						set @_SqlCommand = null ;
+
+						fetch next from addUsersCursor into @_DatabaseUserName, @_DefaultSchema ;
+					end
+
+				close addUsersCursor
+
+				deallocate addUsersCursor
+			end try
+			begin catch
+				select error_message() as [ERROR_MESSAGE];
+				
+				set @_CursorStatus = cursor_status('global','addUsersCursor');
+				select @_CursorStatus as [CURSOR_STATUS];
+
+				if @_CursorStatus >- 1
+					begin
+						close addUsersCursor;
+						deallocate addUsersCursor;
+					end
+				else if @_CursorStatus in (-1, -2)
+					deallocate addUsersCursor;
+
+				select cursor_status('global','addUsersCursor')  as [CURSOR_STATUS];
+
+				throw;
+			end catch
+
+		end
+	else
+		raiserror('[%s] step skipped as disabled', 0, 1, @_Step) ; 
+
+
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+AddRoleMembers:
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	set @_Step = 'Add Role Members' ;
+	if @_DoAddRoleMembers = 1
+		begin
+			if @_Debug = 1
+				select distinct
+					  @_Step as [Step]
+					, u.LoginName
+					, u.Environment
+					, r.RoleName
+					, r.RoleName + coalesce(nullif(s.Environment, 'ALL'), '') as [EffectiveRoleName]
+					, s.ServerName
+					, u.IsActive
+				from
+					#Users as u
+				inner join #RoleMembers as m
+					on m.UserName = u.LoginName
+				inner join #Roles as r
+					on r.RoleName = m.RoleName
+				inner join #Servers as s
+					on s.Environment = r.Environment
+				inner join sys.database_principals as dp
+					on dp.name collate Latin1_General_BIN = u.LoginName collate Latin1_General_BIN
+				where
+						len(u.LoginName) > 0
+					and len(m.RoleName) > 0
+					and u.IsActive = 1
+					and s.ServerName = @_ServerName
+					and u.Environment = @_CurrentEnvironment
+				order by
+					  [LoginName]
+					, [EffectiveRoleName]
+
+			begin try
+				declare addRoleMembersCursor cursor for
+					select distinct
+						  u.LoginName
+						, r.RoleName + coalesce(nullif(s.Environment, 'ALL'), '')
+					from
+						#Users as u
+					inner join #RoleMembers as m
+						on m.UserName = u.LoginName
+					inner join #Roles as r
+						on r.RoleName = m.RoleName
+					inner join #Servers as s
+						on s.Environment = r.Environment
+					inner join sys.database_principals as dp
+						on dp.name collate Latin1_General_BIN = u.LoginName collate Latin1_General_BIN
+					where
+							len(u.LoginName) > 0
+						and len(m.RoleName) > 0
+						and u.IsActive = 1
+						and s.ServerName = @_ServerName
+						and u.Environment = @_CurrentEnvironment ;
+
+				open addRoleMembersCursor
+
+				fetch next from addRoleMembersCursor into @_DatabaseUserName, @_DatabaseRoleName
+
+				while @@fetch_status = 0
+					begin
+						raiserror('Adding user %s to database role %s...', 0, 1, @_DatabaseUserName, @_DatabaseRoleName) with nowait ;
+
+						exec sp_addrolemember @_DatabaseRoleName, @_DatabaseUserName ;
+
+						fetch next from addRoleMembersCursor into @_DatabaseUserName, @_DatabaseRoleName
+					end
+
+				close addRoleMembersCursor
+
+				deallocate addRoleMembersCursor
+			end try
+			begin catch
+				select error_message() as [ERROR_MESSAGE];
+				
+				set @_CursorStatus = cursor_status('global','addRoleMembersCursor');
+				select @_CursorStatus as [CURSOR_STATUS];
+
+				if @_CursorStatus >- 1
+					begin
+						close addRoleMembersCursor;
+						deallocate addRoleMembersCursor;
+					end
+				else if @_CursorStatus in (-1, -2)
+					deallocate addRoleMembersCursor;
+
+				select cursor_status('global','addRoleMembersCursor')  as [CURSOR_STATUS];
+
+				throw;
+			end catch
+
+		end
+	else
+		raiserror('[%s] step skipped as disabled', 0, 1, @_Step) ; 
+
+	--raiserror('ERROR THROWN TO FORCE ROLLBACK', 16, 1) ;
+
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+EndEx:
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	if @@trancount > 0 commit transaction;
+	raiserror('Successfully completed application of non-app user permissions', 0, 1) with nowait ;
+end try
+begin catch
+	if @@trancount > 0 rollback transaction;
+
+	select error_number() as [ERROR_NUMBER], error_message() as [ERROR_MESSAGE];
+
+	throw;
+end catch
+
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+CleanUp:
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+go
+
+if object_id(N'tempdb..#Servers') is not null drop table #Servers;
+go
+if object_id(N'tempdb..#Roles') is not null drop table #Roles;
+go
+if object_id(N'tempdb..#Users') is not null drop table #Users;
+go
+if object_id(N'tempdb..#RoleMembers') is not null drop table #RoleMembers;
+go

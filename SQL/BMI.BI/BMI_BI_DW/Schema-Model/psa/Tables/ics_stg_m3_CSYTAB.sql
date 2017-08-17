94cf27e627edd12c0674a7ca3869d009dc345314
gregmlucas <greg.lucas@icopal.com>
2017-08-17 13:09:38 +0100

    Added PSA and TSA tables for M3

---
+++ SQL/BMI.BI/BMI_BI_DW/Schema-Model/psa/Tables/ics_stg_m3_CSYTAB.sql
@@ -0,0 +1,33 @@
+CREATE TABLE [psa].[ics_stg_m3_CSYTAB]
+(
+[EtlRecordId] [bigint] NOT NULL IDENTITY(1, 1),
+[EtlBatchRunId] [int] NOT NULL,
+[EtlStepRunId] [int] NOT NULL,
+[EtlThreadRunId] [int] NOT NULL,
+[DataSourceKey] [int] NOT NULL,
+[EtlSourceTable] [varchar] (200) NOT NULL,
+[EtlCreatedOn] [datetime] NOT NULL,
+[EtlCreatedBy] [varchar] (200) NOT NULL,
+[EtlUpdatedOn] [datetime] NOT NULL,
+[EtlUpdatedBy] [varchar] (200) NOT NULL,
+[EtlDeletedOn] [datetime] NULL,
+[EtlDeletedBy] [varchar] (200) NULL,
+[IsDeleted] [char] (1) NOT NULL CONSTRAINT [DF_psa_ics_stg_m3_CSYTAB_IsDeleted] DEFAULT ('N'),
+[IsIncomplete] [char] (1) NOT NULL CONSTRAINT [DF_psa_ics_stg_m3_CSYTAB_IsIncomplete] DEFAULT ('N'),
+[CTCONO] [decimal] (3, 0) NULL,
+[CTDIVI] [nvarchar] (255) NULL,
+[CTSTCO] [nvarchar] (255) NULL,
+[CTSTKY] [nvarchar] (255) NULL,
+[CTLNCD] [nvarchar] (255) NULL,
+[CTTX40] [nvarchar] (255) NULL,
+[CTTX15] [nvarchar] (255) NULL,
+[CTPARM] [nvarchar] (255) NULL,
+[CTTXID] [decimal] (13, 0) NULL,
+[CTRGDT] [decimal] (8, 0) NULL,
+[CTRGTM] [decimal] (6, 0) NULL,
+[CTLMDT] [decimal] (8, 0) NULL,
+[CTCHNO] [decimal] (3, 0) NULL,
+[CTCHID] [nvarchar] (255) NULL,
+[CTLMTS] [decimal] (18, 0) NULL
+)
+GO
---
+++ SQL/BMI.BI/BMI_BI_DW/Schema-Model/tsa/Tables/ics_land_m3_OCHCUS.sql
@@ -0,0 +1,30 @@
+CREATE TABLE [tsa].[ics_land_m3_OCHCUS]
+(
+[EtlBatchRunId] [int] NOT NULL,
+[EtlStepRunId] [int] NOT NULL,
+[EtlThreadRunId] [int] NOT NULL,
+[DataSourceKey] [int] NOT NULL,
+[EtlSourceTable] [varchar] (200) NOT NULL,
+[EtlCreatedOn] [datetime] NOT NULL,
+[EtlCreatedBy] [varchar] (200) NOT NULL,
+[ExcludeFromMerge] [bit] NOT NULL CONSTRAINT [DF_tsa_ics_land_m3_OCHCUS_ExcludeFromMerge] DEFAULT ((0)),
+[IsDuplicate] [bit] NOT NULL CONSTRAINT [DF_tsa_ics_land_m3_OCHCUS_IsDuplicate] DEFAULT ((0)),
+[OSCONO] [decimal] (3, 0) NULL,
+[OSCUNO] [nvarchar] (255) NULL,
+[OSCHCT] [nvarchar] (255) NULL,
+[OSCHL1] [nvarchar] (255) NULL,
+[OSCHL2] [nvarchar] (255) NULL,
+[OSCHL3] [nvarchar] (255) NULL,
+[OSCHL4] [nvarchar] (255) NULL,
+[OSFVDT] [decimal] (8, 0) NULL,
+[OSLVDT] [decimal] (8, 0) NULL,
+[OSCHMB] [nvarchar] (255) NULL,
+[OSMBST] [decimal] (1, 0) NULL,
+[OSTXID] [decimal] (13, 0) NULL,
+[OSRGDT] [decimal] (8, 0) NULL,
+[OSRGTM] [decimal] (6, 0) NULL,
+[OSLMDT] [decimal] (8, 0) NULL,
+[OSCHNO] [decimal] (3, 0) NULL,
+[OSCHID] [nvarchar] (255) NULL
+)
+GO

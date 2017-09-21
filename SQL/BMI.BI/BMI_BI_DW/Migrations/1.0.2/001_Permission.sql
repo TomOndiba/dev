367679e1bcce5110f7dcbd9d274c211c4533169e
RNazir <razia.nazir@icopal.com>
2017-09-21 10:40:04 +0100

    script amended.

---
+++ SQL/BMI.BI/BMI_BI_DW/Migrations/1.0.2/001_Permission.sql
@@ -0,0 +1,9 @@
+﻿-- <Migration ID="04d35d57-5cc1-48b8-a110-f739ab501612" />
+GO
+
+PRINT N'Altering permissions on SCHEMA:: [pbi]'
+GO
+GRANT SELECT ON SCHEMA:: [pbi] TO [AppDevelopersPROD]
+GO
+GRANT SELECT ON SCHEMA:: [pbi] TO [BusinessAnalystsPROD]
+GO
--- SQL/BMI.BI/BMI_BI_DW/Migrations/1.0.2/002_Create_Schema_And_Tables.sql
+++ SQL/BMI.BI/BMI_BI_DW/Migrations/1.0.2/002_Create_Schema_And_Tables.sql
@@ -1,11 +1,4 @@
-﻿-- <Migration ID="04d35d57-5cc1-48b8-a110-f739ab501612" />
-GO
-
-PRINT N'Altering permissions on SCHEMA:: [pbi]'
-GO
-GRANT SELECT ON SCHEMA:: [pbi] TO [AppDevelopersPROD]
-GO
-GRANT SELECT ON SCHEMA:: [pbi] TO [BusinessAnalystsPROD]
+﻿-- <Migration ID="a5e13413-bec8-4799-8027-bb9cf29efd12" />
 GO
 ------------------------create schema and tables
 /*    ==Scripting Parameters==

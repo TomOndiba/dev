08b7bc6f3428ecf03008fbf7090b22416e4c83c3
RNazir <razia.nazir@icopal.com>
2017-09-21 10:31:55 +0100

    scripts added and deleted.

--- SQL/BMI.BI/BMI_BI_DW/Migrations/1.0.3/005_Add_Columns_To_psaTotsaLoadControlTable.sql
+++
@@ -1,9 +0,0 @@
-﻿-- <Migration ID="b2fc2f26-37f0-459b-b257-fdaf04410f2d" />
-GO
-
-PRINT N'Altering [dbo].[psaTotsaLoadControlTable]'
-GO
-ALTER TABLE [dbo].[psaTotsaLoadControlTable] ADD
-[SourcePK] [varchar] (max) NULL,
-[TargetPK] [varchar] (max) NULL
-GO
--- SQL/BMI.BI/BMI_BI_DW/Migrations/1.0.3/006_Create_schema_IcsApp.sql
+++
@@ -1,8 +0,0 @@
-﻿-- <Migration ID="d7e0a338-4066-4529-8180-9cf66a7b41ee" />
-GO
-
-PRINT N'Creating schemas'
-GO
-CREATE SCHEMA [IcsApp]
-AUTHORIZATION [dbo]
-GO

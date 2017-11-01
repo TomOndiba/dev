a64f983daa880560c520cf7f4d6ba10ffc36a288
RNazir <razia.nazir@icopal.com>
2017-11-01 13:51:09 +0000

    inventory_snapshot_sap_gbr view added.

---
+++ SQL/BMI.BI/BMI_BI_DW/Migrations/1.0.8/005_Add_inventory_snapshot_sap_gbr_View.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="8865e78b-a9f9-415c-be7d-55959e638939" />
+GO
+
+PRINT N'Dropping [dbo].[inventory_snapshot_sap_gbr]'
+GO
+IF OBJECT_ID(N'[dbo].[inventory_snapshot_sap_gbr]', 'V') IS NOT NULL
+DROP VIEW [dbo].[inventory_snapshot_sap_gbr]
+GO

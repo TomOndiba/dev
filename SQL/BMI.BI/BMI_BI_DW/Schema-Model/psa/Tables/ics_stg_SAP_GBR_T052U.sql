8ba3c02ef24ddc7103fa04932687cb1c3a30b1e6
RNazir <razia.nazir@icopal.com>
2017-11-10 13:02:29 +0000

    psa SAP Procurement tables added.

---
+++ SQL/BMI.BI/BMI_BI_DW/Schema-Model/psa/Tables/ics_stg_SAP_GBR_T052U.sql
@@ -0,0 +1,25 @@
+CREATE TABLE [psa].[ics_stg_SAP_GBR_T052U]
+(
+[EtlRecordId] [bigint] NOT NULL IDENTITY(1, 1),
+[EtlBatchRunId] [int] NOT NULL,
+[EtlStepRunId] [int] NOT NULL,
+[EtlThreadRunId] [int] NOT NULL,
+[DataSourceKey] [int] NOT NULL,
+[EtlSourceTable] [varchar] (200) NOT NULL,
+[EtlCreatedOn] [datetime] NOT NULL,
+[EtlCreatedBy] [varchar] (200) NOT NULL,
+[EtlUpdatedOn] [datetime] NOT NULL,
+[EtlUpdatedBy] [varchar] (200) NOT NULL,
+[EtlDeletedOn] [datetime] NULL,
+[EtlDeletedBy] [varchar] (200) NULL,
+[IsDeleted] [char] (1) NOT NULL CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_T052U_IsDeleted] DEFAULT ('N'),
+[IsIncomplete] [char] (1) NOT NULL CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_T052U_IsIncomplete] DEFAULT ('N'),
+[MANDT] [nvarchar] (3) NOT NULL,
+[SPRAS] [nvarchar] (1) NOT NULL,
+[ZTERM] [nvarchar] (4) NOT NULL,
+[ZTAGG] [nvarchar] (2) NOT NULL,
+[TEXT1] [nvarchar] (50) NULL
+)
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_T052U] ADD CONSTRAINT [psa_ics_stg_SAP_GBR_T052U_PK] PRIMARY KEY CLUSTERED  ([MANDT], [SPRAS], [ZTERM], [ZTAGG])
+GO
---
+++ SQL/BMI.BI/BMI_BI_DW/Schema-Model/psa/Tables/ics_stg_SAP_GBR_TCURR.sql
@@ -0,0 +1,26 @@
+CREATE TABLE [psa].[ics_stg_SAP_GBR_TCURR]
+(
+[EtlRecordId] [bigint] NOT NULL IDENTITY(1, 1),
+[EtlBatchRunId] [int] NOT NULL,
+[EtlStepRunId] [int] NOT NULL,
+[EtlThreadRunId] [int] NOT NULL,
+[DataSourceKey] [int] NOT NULL,
+[EtlSourceTable] [varchar] (200) NOT NULL,
+[EtlCreatedOn] [datetime] NOT NULL,
+[EtlCreatedBy] [varchar] (200) NOT NULL,
+[EtlUpdatedOn] [datetime] NOT NULL,
+[EtlUpdatedBy] [varchar] (200) NOT NULL,
+[EtlDeletedOn] [datetime] NULL,
+[EtlDeletedBy] [varchar] (200) NULL,
+[IsDeleted] [char] (1) NOT NULL CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_TCURR_IsDeleted] DEFAULT ('N'),
+[IsIncomplete] [char] (1) NOT NULL CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_TCURR_IsIncomplete] DEFAULT ('N'),
+[MANDT] [nvarchar] (3) NOT NULL,
+[KURST] [nvarchar] (4) NOT NULL,
+[FCURR] [nvarchar] (5) NOT NULL,
+[TCURR] [nvarchar] (5) NOT NULL,
+[GDATU] [nvarchar] (8) NOT NULL,
+[UKURS] [decimal] (9, 5) NULL,
+[FFACT] [decimal] (9, 0) NULL,
+[TFACT] [decimal] (9, 0) NULL
+)
+GO

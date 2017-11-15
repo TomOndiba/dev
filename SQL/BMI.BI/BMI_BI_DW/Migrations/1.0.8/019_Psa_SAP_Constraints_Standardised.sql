e98206951b3cc70994b226b5bda3684bbdc69dc2
RNazir <razia.nazir@icopal.com>
2017-11-15 10:30:43 +0000

    SAP constraints standardised.

---
+++ SQL/BMI.BI/BMI_BI_DW/Migrations/1.0.8/019_Psa_SAP_Constraints_Standardised.sql
@@ -0,0 +1,171 @@
+﻿-- <Migration ID="cec44a59-fe9b-4ac1-b60f-c607efe55da0" />
+GO
+
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_ADRC]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_ADRC] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_ADRC_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_ADRC]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_ADRC] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_ADRC_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_EKBE]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_EKBE] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_EKBE_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_EKBE]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_EKBE] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_EKBE_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_EKET]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_EKET] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_EKET_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_EKET]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_EKET] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_EKET_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_EKKO]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_EKKO] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_EKKO_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_EKKO]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_EKKO] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_EKKO_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_EKPO]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_EKPO] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_EKPO_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_EKPO]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_EKPO] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_EKPO_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_LFA1]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_LFA1] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_LFA1_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_LFA1]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_LFA1] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_LFA1_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_LFB1]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_LFB1] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_LFB1_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_LFB1]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_LFB1] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_LFB1_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_LFM1]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_LFM1] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_LFM1_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_LFM1]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_LFM1] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_LFM1_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_LIKP]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_LIKP] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_LIKP_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_LIKP]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_LIKP] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_LIKP_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_MARM]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_MARM] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_MARM_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_MARM]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_MARM] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_MARM_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_T052U]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_T052U] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_T052U_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_T052U]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_T052U] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_T052U_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_TCURR]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_TCURR] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_TCURR_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_SAP_GBR_TCURR]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_TCURR] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_SAP_GBR_TCURR_IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_SAP_GBR_ADRC]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_ADRC] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_ADRC_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_ADRC] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_ADRC_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_SAP_GBR_EKBE]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_EKBE] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_EKBE_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_EKBE] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_EKBE_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_SAP_GBR_EKET]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_EKET] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_EKET_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_EKET] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_EKET_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_SAP_GBR_EKKO]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_EKKO] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_EKKO_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_EKKO] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_EKKO_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_SAP_GBR_EKPO]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_EKPO] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_EKPO_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_EKPO] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_EKPO_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_SAP_GBR_LFA1]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_LFA1] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_LFA1_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_LFA1] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_LFA1_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_SAP_GBR_LFB1]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_LFB1] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_LFB1_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_LFB1] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_LFB1_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_SAP_GBR_LFM1]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_LFM1] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_LFM1_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_LFM1] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_LFM1_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_SAP_GBR_LIKP]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_LIKP] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_LIKP_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_LIKP] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_LIKP_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_SAP_GBR_MARM]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_MARM] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_MARM_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_MARM] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_MARM_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_SAP_GBR_T052U]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_T052U] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_T052U_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_T052U] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_T052U_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_SAP_GBR_TCURR]'
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_TCURR] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_TCURR_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_SAP_GBR_TCURR] ADD CONSTRAINT [DF_psa_ics_stg_SAP_GBR_TCURR_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO

68aa56e02de5d2c6035afb1bc58d83409e371cc3
RNazir <razia.nazir@icopal.com>
2017-12-05 11:00:23 +0000

    psa constraints for m3, m3v7 and moves added.

---
+++ SQL/BMI.BI/BMI_BI_DW/Migrations/1.1.0/006_Psa_M3_M3V7_Movex_Constraints_Standardised.sql
@@ -0,0 +1,171 @@
+﻿-- <Migration ID="370fcb23-bbf1-49ac-b337-c68844dc894b" />
+GO
+
+PRINT N'Dropping constraints from [psa].[ics_stg_m3_FGINLI]'
+GO
+ALTER TABLE [psa].[ics_stg_m3_FGINLI] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_m3_FGINLI_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_m3_FGINLI]'
+GO
+ALTER TABLE [psa].[ics_stg_m3_FGINLI] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_m3_FGINLI_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_m3_FPLEDG]'
+GO
+ALTER TABLE [psa].[ics_stg_m3_FPLEDG] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_m3_FPLEDG_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_m3_FPLEDG]'
+GO
+ALTER TABLE [psa].[ics_stg_m3_FPLEDG] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_m3_FPLEDG_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_m3_MPLIND]'
+GO
+ALTER TABLE [psa].[ics_stg_m3_MPLIND] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_m3_MPLIND_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_m3_MPLIND]'
+GO
+ALTER TABLE [psa].[ics_stg_m3_MPLIND] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_m3_MPLIND_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_m3_MPLINE]'
+GO
+ALTER TABLE [psa].[ics_stg_m3_MPLINE] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_m3_MPLINE_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_m3_MPLINE]'
+GO
+ALTER TABLE [psa].[ics_stg_m3_MPLINE] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_m3_MPLINE_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_m3v7_FGINLI]'
+GO
+ALTER TABLE [psa].[ics_stg_m3v7_FGINLI] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_m3v7_FGINLI_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_m3v7_FGINLI]'
+GO
+ALTER TABLE [psa].[ics_stg_m3v7_FGINLI] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_m3v7_FGINLI_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_m3v7_FPLEDG]'
+GO
+ALTER TABLE [psa].[ics_stg_m3v7_FPLEDG] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_m3v7_FPLEDG_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_m3v7_FPLEDG]'
+GO
+ALTER TABLE [psa].[ics_stg_m3v7_FPLEDG] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_m3v7_FPLEDG_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_m3v7_MPLIND]'
+GO
+ALTER TABLE [psa].[ics_stg_m3v7_MPLIND] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_m3v7_MPLIND_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_m3v7_MPLIND]'
+GO
+ALTER TABLE [psa].[ics_stg_m3v7_MPLIND] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_m3v7_MPLIND_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_m3v7_MPLINE]'
+GO
+ALTER TABLE [psa].[ics_stg_m3v7_MPLINE] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_m3v7_MPLINE_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_m3v7_MPLINE]'
+GO
+ALTER TABLE [psa].[ics_stg_m3v7_MPLINE] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_m3v7_MPLINE_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_movex_FGINLI]'
+GO
+ALTER TABLE [psa].[ics_stg_movex_FGINLI] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_movex_FGINLI_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_movex_FGINLI]'
+GO
+ALTER TABLE [psa].[ics_stg_movex_FGINLI] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_movex_FGINLI_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_movex_FPLEDG]'
+GO
+ALTER TABLE [psa].[ics_stg_movex_FPLEDG] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_movex_FPLEDG_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_movex_FPLEDG]'
+GO
+ALTER TABLE [psa].[ics_stg_movex_FPLEDG] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_movex_FPLEDG_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_movex_MPLIND]'
+GO
+ALTER TABLE [psa].[ics_stg_movex_MPLIND] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_movex_MPLIND_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_movex_MPLIND]'
+GO
+ALTER TABLE [psa].[ics_stg_movex_MPLIND] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_movex_MPLIND_IsIncomplete]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_movex_MPLINE]'
+GO
+ALTER TABLE [psa].[ics_stg_movex_MPLINE] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_movex_MPLINE_IsDeleted]
+GO
+PRINT N'Dropping constraints from [psa].[ics_stg_movex_MPLINE]'
+GO
+ALTER TABLE [psa].[ics_stg_movex_MPLINE] DROP CONSTRAINT [DF_psa_ics_stg_ics_stg_movex_MPLINE_IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_m3_FGINLI]'
+GO
+ALTER TABLE [psa].[ics_stg_m3_FGINLI] ADD CONSTRAINT [DF_psa_ics_stg_m3_FGINLI_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_m3_FGINLI] ADD CONSTRAINT [DF_psa_ics_stg_m3_FGINLI_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_m3_FPLEDG]'
+GO
+ALTER TABLE [psa].[ics_stg_m3_FPLEDG] ADD CONSTRAINT [DF_psa_ics_stg_m3_FPLEDG_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_m3_FPLEDG] ADD CONSTRAINT [DF_psa_ics_stg_m3_FPLEDG_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_m3_MPLIND]'
+GO
+ALTER TABLE [psa].[ics_stg_m3_MPLIND] ADD CONSTRAINT [DF_psa_ics_stg_m3_MPLIND_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_m3_MPLIND] ADD CONSTRAINT [DF_psa_ics_stg_m3_MPLIND_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_m3_MPLINE]'
+GO
+ALTER TABLE [psa].[ics_stg_m3_MPLINE] ADD CONSTRAINT [DF_psa_ics_stg_m3_MPLINE_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_m3_MPLINE] ADD CONSTRAINT [DF_psa_ics_stg_m3_MPLINE_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_m3v7_FGINLI]'
+GO
+ALTER TABLE [psa].[ics_stg_m3v7_FGINLI] ADD CONSTRAINT [DF_psa_ics_stg_m3v7_FGINLI_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_m3v7_FGINLI] ADD CONSTRAINT [DF_psa_ics_stg_m3v7_FGINLI_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_m3v7_FPLEDG]'
+GO
+ALTER TABLE [psa].[ics_stg_m3v7_FPLEDG] ADD CONSTRAINT [DF_psa_ics_stg_m3v7_FPLEDG_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_m3v7_FPLEDG] ADD CONSTRAINT [DF_psa_ics_stg_m3v7_FPLEDG_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_m3v7_MPLIND]'
+GO
+ALTER TABLE [psa].[ics_stg_m3v7_MPLIND] ADD CONSTRAINT [DF_psa_ics_stg_m3v7_MPLIND_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_m3v7_MPLIND] ADD CONSTRAINT [DF_psa_ics_stg_m3v7_MPLIND_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_m3v7_MPLINE]'
+GO
+ALTER TABLE [psa].[ics_stg_m3v7_MPLINE] ADD CONSTRAINT [DF_psa_ics_stg_m3v7_MPLINE_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_m3v7_MPLINE] ADD CONSTRAINT [DF_psa_ics_stg_m3v7_MPLINE_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_movex_FGINLI]'
+GO
+ALTER TABLE [psa].[ics_stg_movex_FGINLI] ADD CONSTRAINT [DF_psa_ics_stg_movex_FGINLI_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_movex_FGINLI] ADD CONSTRAINT [DF_psa_ics_stg_movex_FGINLI_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_movex_FPLEDG]'
+GO
+ALTER TABLE [psa].[ics_stg_movex_FPLEDG] ADD CONSTRAINT [DF_psa_ics_stg_movex_FPLEDG_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_movex_FPLEDG] ADD CONSTRAINT [DF_psa_ics_stg_movex_FPLEDG_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_movex_MPLIND]'
+GO
+ALTER TABLE [psa].[ics_stg_movex_MPLIND] ADD CONSTRAINT [DF_psa_ics_stg_movex_MPLIND_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_movex_MPLIND] ADD CONSTRAINT [DF_psa_ics_stg_movex_MPLIND_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO
+PRINT N'Adding constraints to [psa].[ics_stg_movex_MPLINE]'
+GO
+ALTER TABLE [psa].[ics_stg_movex_MPLINE] ADD CONSTRAINT [DF_psa_ics_stg_movex_MPLINE_IsDeleted] DEFAULT ('N') FOR [IsDeleted]
+GO
+ALTER TABLE [psa].[ics_stg_movex_MPLINE] ADD CONSTRAINT [DF_psa_ics_stg_movex_MPLINE_IsIncomplete] DEFAULT ('N') FOR [IsIncomplete]
+GO

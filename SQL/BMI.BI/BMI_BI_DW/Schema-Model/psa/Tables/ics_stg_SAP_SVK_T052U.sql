855d3326d57e159854e6802201768976206ca73c
RNazir <razia.nazir@icopal.com>
2017-12-05 12:41:31 +0000

    psa table for SAP SVK added.

---
+++ SQL/BMI.BI/BMI_BI_DW/Schema-Model/psa/Tables/ics_stg_SAP_SVK_T052U.sql
@@ -0,0 +1,25 @@
+CREATE TABLE [psa].[ics_stg_SAP_SVK_T052U]
+(
+[EtlRecordId] [bigint] NOT NULL IDENTITY(1, 1),
+[EtlBatchRunId] [int] NOT NULL,
+[EtlStepRunId] [int] NOT NULL,
+[EtlThreadRunId] [int] NOT NULL,
+[DataSourceKey] [int] NOT NULL,
+[EtlSourceTable] [varchar] (200) NOT NULL,
+[EtlCreatedOn] [datetime] NOT NULL,
+[EtlCreatedBy] [varchar] (200) NOT NULL,
+[EtlUpdatedOn] [datetime] NOT NULL,
+[EtlUpdatedBy] [varchar] (200) NOT NULL,
+[EtlDeletedOn] [datetime] NULL,
+[EtlDeletedBy] [varchar] (200) NULL,
+[IsDeleted] [char] (1) NOT NULL CONSTRAINT [DF_psa_ics_stg_SAP_SVK_T052U_IsDeleted] DEFAULT ('N'),
+[IsIncomplete] [char] (1) NOT NULL CONSTRAINT [DF_psa_ics_stg_SAP_SVK_T052U_IsIncomplete] DEFAULT ('N'),
+[MANDT] [nvarchar] (3) NOT NULL,
+[SPRAS] [nvarchar] (1) NOT NULL,
+[TEXT1] [nvarchar] (50) NULL,
+[ZTAGG] [nvarchar] (2) NOT NULL,
+[ZTERM] [nvarchar] (4) NOT NULL
+)
+GO
+ALTER TABLE [psa].[ics_stg_SAP_SVK_T052U] ADD CONSTRAINT [PK_psa_ics_stg_SAP_SVK_T052U] PRIMARY KEY CLUSTERED  ([ZTAGG], [ZTERM], [SPRAS], [MANDT])
+GO

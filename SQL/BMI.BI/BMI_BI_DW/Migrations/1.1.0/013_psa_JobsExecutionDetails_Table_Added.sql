238f8f7d45d9653c179c2fb5d8db8f8878b0ca6e
RNazir <razia.nazir@icopal.com>
2017-12-21 14:08:44 +0000

    Psa JobExecutionDetails table added.

---
+++ SQL/BMI.BI/BMI_BI_DW/Migrations/1.1.0/013_psa_JobsExecutionDetails_Table_Added.sql
@@ -0,0 +1,24 @@
+﻿-- <Migration ID="8ea5bee5-7f37-4bd0-86fe-e00233ef3cbb" />
+GO
+
+PRINT N'Creating [psa].[JobsExecutionDetails]'
+GO
+CREATE TABLE [psa].[JobsExecutionDetails]
+(
+[Id] [int] NOT NULL,
+[Action] [nvarchar] (10) NULL,
+[MCTName] [nvarchar] (100) NULL,
+[MCTStartTime] [datetime] NULL,
+[MCTEndTime] [datetime] NULL,
+[MCTTotalExecutionTime] [varchar] (25) NULL,
+[SuccessSourceRows] [int] NULL,
+[SuccessTargetRows] [int] NULL,
+[FailedSourceRows] [int] NULL,
+[FailedTargetRows] [int] NULL,
+[CDCNoOfIteration] [int] NOT NULL
+)
+GO
+PRINT N'Creating primary key [PK_Id_JobsExecutionDetails] on [psa].[JobsExecutionDetails]'
+GO
+ALTER TABLE [psa].[JobsExecutionDetails] ADD CONSTRAINT [PK_Id_JobsExecutionDetails] PRIMARY KEY CLUSTERED  ([Id])
+GO

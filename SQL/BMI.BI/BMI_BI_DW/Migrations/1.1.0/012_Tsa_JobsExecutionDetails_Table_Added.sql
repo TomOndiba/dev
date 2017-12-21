57dccf7563d2aee3fdef0f9adc0532c3df5406c1
RNazir <razia.nazir@icopal.com>
2017-12-21 14:03:11 +0000

    jobexecutiondetails tsa table added.

---
+++ SQL/BMI.BI/BMI_BI_DW/Migrations/1.1.0/012_Tsa_JobsExecutionDetails_Table_Added.sql
@@ -0,0 +1,27 @@
+﻿-- <Migration ID="3f3d01a4-f7b1-4427-9e4e-e3c5eaa5cdc1" />
+go
+if object_id('tsa.JobsExecutionDetails', 'U') is not null
+	drop table tsa.JobsExecutionDetails ;
+go
+
+PRINT N'Creating [tsa].[JobsExecutionDetails]'
+GO
+CREATE TABLE [tsa].[JobsExecutionDetails]
+(
+[Id] [int] NOT NULL IDENTITY(1, 1),
+[Action] [nvarchar] (10) NULL CONSTRAINT [JobLoggingDetail_Action_Start] DEFAULT ('RUN'),
+[MCTName] [nvarchar] (100) NULL,
+[MCTStartTime] [datetime] NULL,
+[MCTEndTime] [datetime] NULL,
+[MCTTotalExecutionTime] [varchar] (25) NULL,
+[SuccessSourceRows] [int] NULL,
+[SuccessTargetRows] [int] NULL,
+[FailedSourceRows] [int] NULL,
+[FailedTargetRows] [int] NULL,
+[CDCNoOfIteration] [int] NULL CONSTRAINT [JobsExecutionDetails_CDCNoOfIteration] DEFAULT ((1))
+)
+GO
+PRINT N'Creating primary key [PK_Id_JobsExecutionDetails] on [tsa].[JobsExecutionDetails]'
+GO
+ALTER TABLE [tsa].[JobsExecutionDetails] ADD CONSTRAINT [PK_Id_JobsExecutionDetails] PRIMARY KEY CLUSTERED  ([Id])
+GO

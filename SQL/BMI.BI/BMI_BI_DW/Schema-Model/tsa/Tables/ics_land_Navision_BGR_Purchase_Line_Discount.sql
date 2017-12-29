1e6f3c02e5ab85ea1433edee5d71e4a6b0771ec2
Matthew Basoo <matthew.basoo@bmigroup.com>
2017-12-29 11:16:43 +0000

    Added Navision and passed tests

---
+++ SQL/BMI.BI/BMI_BI_DW/Schema-Model/tsa/Tables/ics_land_Navision_BGR_Purchase_Line_Discount.sql
@@ -0,0 +1,25 @@
+CREATE TABLE [tsa].[ics_land_Navision_BGR_Purchase_Line_Discount]
+(
+[EtlBatchRunId] [int] NOT NULL,
+[EtlStepRunId] [int] NOT NULL,
+[EtlThreadRunId] [int] NOT NULL,
+[DataSourceKey] [int] NOT NULL,
+[EtlSourceTable] [varchar] (200) NOT NULL,
+[EtlCreatedOn] [datetime] NOT NULL,
+[EtlCreatedBy] [varchar] (200) NOT NULL,
+[ExcludeFromMerge] [bit] NOT NULL CONSTRAINT [DF_tsa_ics_land_Navision_BGR_Purchase_Line_Discount_ExcludedFromMerge] DEFAULT ((0)),
+[IsDuplicate] [bit] NOT NULL CONSTRAINT [DF_tsa_ics_land_Navision_BGR_Purchase_Line_Discount_IsDuplicate] DEFAULT ((0)),
+[Currency Code] [varchar] (10) NOT NULL,
+[Ending Date] [datetime] NULL,
+[Item No_] [varchar] (20) NOT NULL,
+[Line Discount %] [decimal] (38, 20) NULL,
+[Minimum Quantity] [decimal] (38, 20) NOT NULL,
+[Starting Date] [datetime] NOT NULL,
+[timestamp] [varbinary] (8) NULL,
+[Unit of Measure Code] [varchar] (10) NOT NULL,
+[Variant Code] [varchar] (10) NOT NULL,
+[Vendor No_] [varchar] (20) NOT NULL
+)
+GO
+CREATE CLUSTERED INDEX [IndClust_tsa_ics_land_Navision_BGR_Purchase_Line_Discount] ON [tsa].[ics_land_Navision_BGR_Purchase_Line_Discount] ([Item No_], [Vendor No_], [Starting Date], [Currency Code], [Variant Code], [Unit of Measure Code], [Minimum Quantity])
+GO
---
+++ SQL/BMI.BI/BMI_BI_DW/Schema-Model/tsa/Tables/ics_land_Navision_ROU_Std_Purchase_Line.sql
@@ -0,0 +1,27 @@
+CREATE TABLE [tsa].[ics_land_Navision_ROU_Std_Purchase_Line]
+(
+[EtlBatchRunId] [int] NOT NULL,
+[EtlStepRunId] [int] NOT NULL,
+[EtlThreadRunId] [int] NOT NULL,
+[DataSourceKey] [int] NOT NULL,
+[EtlSourceTable] [varchar] (200) NOT NULL,
+[EtlCreatedOn] [datetime] NOT NULL,
+[EtlCreatedBy] [varchar] (200) NOT NULL,
+[ExcludeFromMerge] [bit] NOT NULL CONSTRAINT [DF_tsa_ics_land_Navision_ROU_Std_Purchase_Line_ExcludedFromMerge] DEFAULT ((0)),
+[IsDuplicate] [bit] NOT NULL CONSTRAINT [DF_tsa_ics_land_Navision_ROU_Std_Purchase_Line_IsDuplicate] DEFAULT ((0)),
+[Amount Excl_ VAT] [decimal] (38, 20) NULL,
+[Description] [varchar] (50) NULL,
+[Line No_] [int] NOT NULL,
+[No_] [varchar] (20) NULL,
+[Quantity] [decimal] (38, 20) NULL,
+[Shortcut Dimension 1 Code] [varchar] (20) NULL,
+[Shortcut Dimension 2 Code] [varchar] (20) NULL,
+[Standard Purchase Code] [varchar] (10) NOT NULL,
+[timestamp] [varbinary] (8) NULL,
+[Type] [int] NULL,
+[Unit of Measure Code] [varchar] (10) NULL,
+[Variant Code] [varchar] (10) NULL
+)
+GO
+CREATE CLUSTERED INDEX [IndClust_tsa_ics_land_Navision_ROU_Std_Purchase_Line] ON [tsa].[ics_land_Navision_ROU_Std_Purchase_Line] ([Standard Purchase Code], [Line No_])
+GO
